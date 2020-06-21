69616c787647a533bb4850a30246734a212df0ea
ludovitsiket <siket.ludovit@gmail.com>
2020-06-21 10:30:08 +0200

    Functions and connection.

---
+++ switchstat.py
@@ -0,0 +1,49 @@
+import telnetlib
+import csv
+from socket import gaierror
+
+
+def read_file(addr):
+    x = []
+    with open(addr) as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            x.append(row)
+    return x
+
+
+def cred(data):
+    ip, name, pwd = [], [], []
+    x = 0
+    while x < len(data):
+        ip.append(data[x]['address'])
+        name.append(data[x]['name'])
+        pwd.append(data[x]['password'])
+        x += 1
+    return (ip, name, pwd)
+
+
+def telnet_conn(data):
+    try:
+        ip, name, pwd = cred(data)
+        x = 0
+        while x < len(data):
+            print(ip[x], name[x], pwd[x])
+            tn = telnetlib.Telnet(ip[x])
+            print('Connection to ' + ip[x] + ' OK.')
+            tn.close()
+            print('Connection to ' + ip[x] + ' close.')
+            x += 1
+    except (KeyboardInterrupt, FileNotFoundError) as e:
+        print(e)
+    except gaierror as e:
+        print(ip[x], e)
+
+
+def main():
+    addr = 'addr.csv'
+    data = read_file(addr)
+    telnet_conn(data)
+
+
+main()
